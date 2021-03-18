c16c39aa333844fc835ad469574c6401e28dac93
joanfonseca <joan.fonseca01@uptc.edu.co>
2021-03-18 10:15:35 -0500

    Wompi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Pasarela';
+export class AppComponent  {
+  title = 'Pasarela'
+  formGroup: FormGroup
+
+ // constructor (private formBuilder: FormBuilder) {}
+  
+  ngOnInit () {
+    /*LLAVE_PUBLICA_DEL_COMERCIO = 'pub_test_X0zDA9xoKdePzhd8a0x9HAez7HgGO2fH'
+  MONEDA='COP'
+  MONTO_EN_CENTAVOS=7890000
+  REFERENCIA_DE_PAGO='37DNKF84S92N1S'this.formGroup = this.formBuilder.group({
+      LLAVE_PUBLICA_DEL_COMERCIO: new FormControl('', [Validators.required]),
+      MONTO_EN_CENTAVOS: new FormControl('', [Validators.required]),
+      REFERENCIA_DE_PAGO: new FormControl('', [Validators.required]),
+      URL_REDIRECCION: new FormControl('', [Validators.required]),
+      MONEDA: new FormControl('', [Validators.required])
+    })
+    this.formGroup
+      .get('LLAVE_PUBLICA_DEL_COMERCIO')
+      .setValue('pub_test_X0zDA9xoKdePzhd8a0x9HAez7HgGO2fH')
+    this.formGroup.get('MONEDA').setValue('COP')
+    this.formGroup.get('MONTO_EN_CENTAVOS').setValue(7890000)
+    this.formGroup.get('REFERENCIA_DE_PAGO').setValue('37DNKF84S92N1S')
+    this.formGroup
+      .get('URL_REDIRECCION')
+      .setValue('https://transaction-redirect.wompi.co/check')
+  */}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { ButtonModule } from 'primeng/button'
+import { ReactiveFormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms'
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
